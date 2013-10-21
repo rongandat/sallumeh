66906e5cb848338138c082e4d62e113eecd5a102
admin <admin@192.168.0.108>
2013-10-21 16:46:49 +0700

    Commit header and product template

--- wp-content/themes/woostore/single-product/add-to-cart/simple.php
+++ wp-content/themes/woostore/single-product/add-to-cart/simple.php
@@ -37,10 +37,9 @@ if ( ! $product->is_purchasable() ) return;
 //	 				'min_value' => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
 //	 				'max_value' => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product )
 //	 			) );
-	 	?>
+//	 	?>
             <input type="hidden" name="quantity" value="1"/>
-            <a class="cart" href="javascript:void(0)" onclick="jQuery('form').submit()"><?php echo __( 'Add to bag', 'woocommerce' ) ?></a>
-
+            <button type="submit" class="single_add_to_cart_button button alt"><?php echo apply_filters('single_add_to_cart_text', __( 'ADD TO MY BAG', 'woocommerce' ), $product->product_type); ?></button>
 	 	<?php do_action('woocommerce_after_add_to_cart_button'); ?>
 
 	</form>
